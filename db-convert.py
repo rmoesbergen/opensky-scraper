1cf6e3acea11b8218ca23b7b6110224a33ae40c6
Ronald Moesbergen <github@rmoesbergen.nl>
2020-06-18 15:15:30 +0200

    Deal with out-of-bounds time ranges

--- db-convert.py
+++ db-convert.py
@@ -72,6 +72,11 @@ class AudioDatabase:
 
     def find_highest_sample(self, timerange_start, timerange_end):
         # Scan through all the audio samples for this time range, find the max value
+        if timerange_start < 0:
+            timerange_start = 0
+        if timerange_end >= 86400:
+            timerange_end = 86399
+
         max_dba_index = 0
         for index in range(timerange_start, timerange_end):
             sample = self.get_sample(index)
